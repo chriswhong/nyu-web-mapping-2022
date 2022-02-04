1facc4c85735419bc44a95bb0d51ee59adaf530b
Chris Whong <chris.m.whong@gmail.com>
2022-02-03 19:21:27 -0500

    first commit after porting jsfiddle to separate files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+
+$('#sublime').on('click', function() {
+	$(".right-half").html("<img src='https://upload.wikimedia.org/wikipedia/en/9/94/Sublime_Self-Titled.jpg'/>");
+})
+
+$('#weezer').on('click', function() {
+	$(".right-half").html("<img src='https://m.media-amazon.com/images/I/71nYpz++VCL._SL1400_.jpg'/>");
+})
+
+$('#jayz').on('click', function() {
+	$(".right-half").html("<img src='https://upload.wikimedia.org/wikipedia/en/b/bb/Jay-Z_-_The_Blueprint_3.jpg'/>");
+})
